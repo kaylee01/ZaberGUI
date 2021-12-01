b3fe355141a3a2850e7ec41e23a6b27bc932da14
kaylee01 <65952228+kaylee01@users.noreply.github.com>
2021-12-01 18:43:12 +0800

    Update zaberController.py

--- zaberController.py
+++ zaberController.py
@@ -1,3 +1,9 @@
+''' 
+Written by Kaylee Molin (22734429) 
+01/12/21
+'''
+
+
 from zaber_motion import Library, Units
 from zaber_motion.ascii import Connection
 
@@ -18,14 +24,13 @@ import serial
 
 
 with Connection.open_serial_port("/dev/cu.usbmodem1101") as connection:
-    device_list = connection.detect_devices()
+    device_list = connection.detect_devices() 
     print("Found {} devices".format(len(device_list)))
 
     # The rest of your program goes here (indented)
 
     xy_device = device_list[0]
-    z_device = device_list[1] # z
-    #print(device_list[1])
+    z_device = device_list[1]
 
     # homes the device
     axis1 = xy_device.get_axis(1) # x ?
@@ -37,4 +42,17 @@ with Connection.open_serial_port("/dev/cu.usbmodem1101") as connection:
     #axis1.home()
     #axis2.home()
     axis3.home()
-    
+
+    ''' Move the specified axis to an absolute distance (mm)'''
+    def move_abs(axis, distance):
+        if axis != axis1 or axis != axis2 or axis != axis3:
+            print("{} is not a valid axis. Please input axis1, axis2 or axis3.".format(axis))
+        else:
+            axis.move_absolute(distance, Units.LENGTH_MILLIMETRES)
+
+
+def main():
+    move_abs("ds", 5)
+
+if __name__ == "__main()__":
+    main()
