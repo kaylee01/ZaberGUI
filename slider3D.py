aad659a7dad4ee39d12e64cdd22a6efe9a597e79
kaylee01 <65952228+kaylee01@users.noreply.github.com>
2022-01-21 18:16:26 +0800

    working 3d :))

--- slider3D.py
+++ slider3D.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button
-import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
-
+from matplotlib.widgets import Slider, Button, RadioButtons
+import pandas as pd
 
 df = pd.read_csv('/Users/kayleemolin/Desktop/summer_project/ZaberGUI/movementData.csv')
 
@@ -14,25 +13,33 @@ z = df["z"]
 
 init_i = 0
 
-# Create the figure and the line that we will manipulate
+
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+plt.subplots_adjust(bottom=0.25) 
+        
+'''ax = fig.add_subplot(121, projection='3d')        
+X= np.arange(-50,50,2)
+Y=np.arange(-50,50,2)
+X,Y = np.meshgrid(X,Y)
 
-#fig, ax = plt.subplots()
-#line, = plt.plot(x[init_i],y[init_i],z[init_i], "bo")
+Z = np.sqrt((X**2+Y**2)/(np.tan(np.pi/120)))            
+ax.plot_wireframe(X,Y,Z, rstride=3, cstride=3)  '''       
+#plt.axis('scaled')
 
-mylines=ax.plot(x[init_i],y[init_i],z[init_i], "bo")
+h0=0
 
-plt.plot(x,y,z, alpha=0.1)
-#ax.set_xlim(minx,maxx)
-#ax.set_ylim(miny,maxy)
-ax.set_xlabel('x position')
-ax.set_ylabel('y position')
-ax.set_zlabel('z position')
+ax2 = fig.add_subplot(111, projection='3d') 
+
+#Z2 = 0*X+0*Y+h0        
+
+#l=ax2.plot_surface(X,Y,Z2,color='red',rstride=2, cstride=2)
+l=ax2.plot(x[init_i],y[init_i],z[init_i], "bo")
+ax2.plot(x,y,z, alpha=0.1)
+'''axhauteur = plt.axes([0.2, 0.1, 0.65, 0.03])
+shauteur = Slider(axhauteur, 'Hauteur', 0.5, 10.0, valinit=h0)
+'''
 
 
-# adjust the main plot to make room for the sliders
-plt.subplots_adjust(left=0.15, bottom=0.25)
 
 # Make a horizontal slider to control the time.
 axtime = plt.axes([0.15, 0.1, 0.65, 0.03])
@@ -45,33 +52,17 @@ freq_slider = Slider(
     valstep=1
 )
 
-# The function to be called anytime a slider's value changes
-def update(val):
-    #line.set_ydata(f(t, amp_slider.val, freq_slider.val))
-    '''line.set_xdata(x[val])
-    line.set_ydata(y[val])
-    #line.set_zdata(z[val])
-
-    line.set_3d_properties(z[val])'''
-
-    mylines[0].set_xdata(x[val])
-    mylines[0].set_ydata(y[val])
-    mylines[0].set_3d_properties(z[val])
-    
-    fig.canvas.draw_idle()
-
-
-# register the update function with each slider
+def update(val): 
+    h = freq_slider.val 
+    ax2.clear()
+    #l=ax2.plot_surface(X,Y,0*X+0*Y+h,color='red',rstride=2, cstride=2)
+    l=ax2.plot(x[h],y[h],z[h], "bo")
+    ax2.plot(x,y,z, alpha=0.1)
+    ax2.set_xlim(min(x),max(x))
+    ax2.set_ylim(min(y),max(y))
+    ax2.set_zlim(min(z),max(z))
+    fig.canvas.draw_idle() 
 freq_slider.on_changed(update)
-#amp_slider.on_changed(update)
-
-# Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
-
-
-def reset(event):
-    freq_slider.reset()
-button.on_clicked(reset)
+#ax2.set_zlim(0,10)
 
 plt.show()
