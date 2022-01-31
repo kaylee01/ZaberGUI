1f0287994ad5c97731ad4904ea7f71af0263ef47
kaylee01 <65952228+kaylee01@users.noreply.github.com>
2022-01-31 17:21:28 +0800

    subplots added
    3d and 2d plots for displacements

--- slider3dFunction.py
+++ slider3dFunction.py
@@ -13,28 +13,38 @@ import pandas as pd
 def load_csv(csv_path):
     df = pd.read_csv(csv_path)
     t = df["t"]
+    maxt = len(t)-1
     x = df["x"]
     y = df["y"]
     z = df["z"]
 
-    return t,x,y,z
+    return t,x,y,z, maxt
 
-def plotter(x,y,z):
-    fig = plt.figure()
+def plotter(x,y,z,t):
+    #fig = plt.subplot(2, 1, 1)
+    fig = plt.figure(figsize=(8, 8))    # window size
     plt.subplots_adjust(bottom=0.25) 
     
-    ax = fig.add_subplot(111, projection='3d') 
-    ax.set_xlabel('x position')
-    ax.set_ylabel('y position')
-    ax.set_zlabel('z position')
+    ax = fig.add_subplot(212, projection='3d') 
+    ax.set_xlabel('x position (mm)')
+    ax.set_ylabel('y position (mm)')
+    ax.set_zlabel('z position (mm)')
 
     l=ax.plot(x[0],y[0],z[0], "bo")
     ax.plot(x,y,z, alpha=0.2)
 
+    ax2 = fig.add_subplot(211)
+    ax2.set_xlabel('time')
+    ax2.set_ylabel('displacement (mm)')
+    ax2.plot(t, x, label='x')
+    ax2.plot(t, y, label='y')
+    ax2.plot(t, z, label='z')
+    ax2.legend()
+
     return plt, fig, ax
 
 
-def create_slider(plt,fig,ax):
+def create_slider(plt,fig,ax,maxt):
 
     # Make a horizontal slider to control the time.
     axtime = plt.axes([0.15, 0.1, 0.65, 0.03])
@@ -42,27 +52,27 @@ def create_slider(plt,fig,ax):
         ax=axtime,
         label='Time',
         valmin=0,
-        valmax=2500,
+        valmax=maxt,
         valinit=0,
-        valstep=1
+        valstep=1,
     )
 
     return freq_slider
 
 def display(filename):
-    t,x,y,z = load_csv(filename)
+    t,x,y,z,maxt = load_csv(filename)
 
-    plt, fig, ax = plotter(x,y,z)
-    freq_slider = create_slider(plt,fig,ax)
+    plt, fig, ax = plotter(x,y,z,t)
+    freq_slider = create_slider(plt,fig,ax,maxt)
 
     def update(val): 
         h = freq_slider.val 
         ax.clear()
         l=ax.plot(x[h],y[h],z[h], "bo")
         ax.plot(x,y,z, alpha=0.2)
-        ax.set_xlabel('x position')
-        ax.set_ylabel('y position')
-        ax.set_zlabel('z position')
+        ax.set_xlabel('x position (mm)')
+        ax.set_ylabel('y position (mm)')
+        ax.set_zlabel('z position (mm)')
         
         fig.canvas.draw_idle()
         
@@ -71,3 +81,4 @@ def display(filename):
     plt.show()
 
 display('/Users/kayleemolin/Desktop/summer_project/ZaberGUI/movementData.csv')
+#display('/Users/kayleemolin/Desktop/summer_project/ZaberGUI/movementData2.csv')
