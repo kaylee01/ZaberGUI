6326f5df72978b6c479e4f0368c7c2451bfc8344
kaylee01 <65952228+kaylee01@users.noreply.github.com>
2022-01-31 16:04:16 +0800

    plotter code only

---
+++ slider3dFunction.py
@@ -0,0 +1,73 @@
+'''
+Written by Kaylee Molin 21/01/2022
+
+Plots the position of a tumour in the x, y and z plane. Slider for time.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.widgets import Slider, Button, RadioButtons
+import pandas as pd
+
+def load_csv(csv_path):
+    df = pd.read_csv(csv_path)
+    t = df["t"]
+    x = df["x"]
+    y = df["y"]
+    z = df["z"]
+
+    return t,x,y,z
+
+def plotter(x,y,z):
+    fig = plt.figure()
+    plt.subplots_adjust(bottom=0.25) 
+    
+    ax = fig.add_subplot(111, projection='3d') 
+    ax.set_xlabel('x position')
+    ax.set_ylabel('y position')
+    ax.set_zlabel('z position')
+
+    l=ax.plot(x[0],y[0],z[0], "bo")
+    ax.plot(x,y,z, alpha=0.2)
+
+    return plt, fig, ax
+
+
+def create_slider(plt,fig,ax):
+
+    # Make a horizontal slider to control the time.
+    axtime = plt.axes([0.15, 0.1, 0.65, 0.03])
+    freq_slider = Slider(
+        ax=axtime,
+        label='Time',
+        valmin=0,
+        valmax=2500,
+        valinit=0,
+        valstep=1
+    )
+
+    return freq_slider
+
+def display(filename):
+    t,x,y,z = load_csv(filename)
+
+    plt, fig, ax = plotter(x,y,z)
+    freq_slider = create_slider(plt,fig,ax)
+
+    def update(val): 
+        h = freq_slider.val 
+        ax.clear()
+        l=ax.plot(x[h],y[h],z[h], "bo")
+        ax.plot(x,y,z, alpha=0.2)
+        ax.set_xlabel('x position')
+        ax.set_ylabel('y position')
+        ax.set_zlabel('z position')
+        
+        fig.canvas.draw_idle()
+        
+    freq_slider.on_changed(update)
+
+    plt.show()
+
+display('/Users/kayleemolin/Desktop/summer_project/ZaberGUI/movementData.csv')
