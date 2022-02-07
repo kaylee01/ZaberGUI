505fdb4c67c3e0a6ecd066bf8c7e580f18e5782f
kaylee01 <65952228+kaylee01@users.noreply.github.com>
2022-02-07 16:07:38 +0800

    working code
    you plug in the device and it will show up on the drop down

--- serial.py
+++ serial.py
@@ -1,25 +1,29 @@
 '''
 Written by Kaylee Molin (22734429) 2/2/22
-Popup screen where youcan select the serail connection for the zaber platform
+Popup screen where you can select the serial connection for the zaber platform
 '''
 
 import os 
 import PySimpleGUI as sg
 import glob
+import time
 
 sg.theme('Reddit')
 SERIAL_PATH = ''
-test_list = ["hi", "kaylee", "this", "is", "a", "list", "stupid"]
 
-#input("Make sure device is removed, then press a key...") 
-b4 = os.listdir("/dev") 
-#input("Now plug in the device, then hit any key...") 
-after = os.listdir("/dev") 
-#print("Newly plugged in devices:\n") 
-for line in after: 
-    if line not in b4: 
-        #print(line)
-        SERIAL_PATH = line
+
+def get_new_connections():
+    input("Make sure device is removed, then press a key...") 
+    before = os.listdir("/dev") 
+    input("Now plug in the device, then hit any key...") 
+    after = os.listdir("/dev") 
+    print("Newly plugged in devices:\n") 
+    for line in after: 
+        if line not in before: 
+            print(line)
+            SERIAL_PATH = line
+
+
 
 def get_connections():
     tty = glob.glob('/dev/tty*')
@@ -33,9 +37,17 @@ layoutSerial = [
     [sg.Button('OK', key='-OKSERIAL-'), sg.Exit()]
           ]
 
-window = sg.Window('Window Title', layoutSerial)    
+layoutSerial2 = [
+    [sg.Text('Make sure the device is unplugged then click \'OK\'', key='-TEXT-',font = 'Any 16')],
+    #[sg.Text('Alternatively, select the device from the list below if the connection port is alreadt known', key='-TEXT2-',font = 'Any 16')],
+    [sg.Combo(SERIAL_PATH, size=(40,10), font = 'Any 16', enable_events=False, key='-CONNECTIONS-')],
+    [sg.Button('OK', key='-OKSERIAL-'), sg.Exit()]
+          ]
 
+window = sg.Window('Serial Connection', layoutSerial)    
 
+ok_count = 0
+connection_list = []
 
 while True:
         event, values = window.read()
@@ -44,6 +56,35 @@ while True:
             break
 
         if event == '-OKSERIAL-':
+            ok_count += 1
             SERIAL_PATH = values['-CONNECTIONS-']
             print(SERIAL_PATH)
-            window.close()
+            
+            if ok_count == 1:
+                window["-TEXT-"].update("Now plug in your device and click \'OK\'.")
+                time.sleep(1)
+                before = os.listdir("/dev") 
+                
+            
+            if ok_count == 2:
+                window["-TEXT-"].update("Select your device from the dropdown menu.")
+                time.sleep(1)
+                after = os.listdir("/dev") 
+                
+                for line in after: 
+                    if line not in before and line.startswith('cu'): 
+                        print(line)
+                        #SERIAL_PATH = line
+                        connection_list.append(line)
+                window['-CONNECTIONS-'].update(value='', values=connection_list)
+                
+            if ok_count == 3:
+                SERIAL_PATH = "/dev/" + values['-CONNECTIONS-']
+                print("...." + SERIAL_PATH)
+                window.close()
+                
+
+
+            
+
+            
